93a75c48c2b11d0adc285c35fa0c0a834e9fa661
kiu-chan <baokhanhntby@gmail.com>
2023-11-19 03:04:58 +0700

    Login + Signup

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,12 +2,16 @@ import { Route, Routes } from "react-router-dom";
 
 import LandingPage from "../pages/Landing";
 import AdminPage from "../pages/Admin";
+import LoginPage from "../pages/Account/Login/index";
+import SignupPage from "../pages/Account/SignUp/index";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />}></Route>
       <Route path="/admin" element={<AdminPage />}></Route>
+      <Route path="/login" element={<LoginPage />}></Route>
+      <Route path="/signup" element={<SignupPage />}></Route>
       {/* <Route path="*" element={<NotFoundPage />}></Route> */}
     </Routes>
   );
